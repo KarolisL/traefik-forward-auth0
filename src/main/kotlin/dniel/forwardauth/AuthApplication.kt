f71a360fffb91935d5ddef37f78944d83baddff5
Daniel <daniel@engfeldt.net>
2019-03-16 10:37:34 +0100

    #54 Add javadoc

--- src/main/kotlin/dniel/forwardauth/AuthApplication.kt
+++ src/main/kotlin/dniel/forwardauth/AuthApplication.kt
@@ -12,6 +12,8 @@ import org.springframework.boot.runApplication
 
 
 /**
+ * Main class for ForwardAuth.
+ *
  * 8888888888                                                  888        d8888          888    888
  * 888                                                         888       d88888          888    888
  * 888                                                         888      d88P888          888    888
@@ -21,21 +23,23 @@ import org.springframework.boot.runApplication
  * 888     Y88..88P 888     Y88b 888 d88P 888  888 888    Y88b 888  d8888888888 Y88b 888 Y88b.  888  888
  * 888      "Y88P"  888      "Y8888888P"  "Y888888 888     "Y88888 d88P     888  "Y88888  "Y888 888  888
  *
- * ${Ansi.RED} :: Version ${spring-boot.formatted-version} :: ${Ansi.DEFAULT}
- *
- *
  */
 @SpringBootApplication
 @EnableConfigurationProperties(AuthProperties::class)
 class AuthApplication(val auth: AuthProperties) : ResourceConfig() {
     private val LOGGER = LoggerFactory.getLogger(this.javaClass)
 
+    /**
+     * Because Jersey does not auto register its classes with Spring
+     * We need ot do it manually.
+     */
     init {
         register(AppExceptionMapper::class.java)
         register(ServerFaultExceptionMapper::class.java)
         register(AuthorizeEndpoint::class.java)
         register(SigninEndpoint::class.java)
 
+        // TODO implement more advanced logic for dumping current loaded application config.
         LOGGER.info(auth.toString());
     }
 
